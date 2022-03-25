065a71eba7993dc88579121b0596eb6daf2e8ec8
wilsonpan20 <wilson.evangelista22@gmail.com>
2022-03-25 13:37:35 -0300

    melhorando o codigo

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -10,7 +10,7 @@ const About = () => {
                 <div className='col-2'>
                     <h2>Sobre</h2>
                     <span className='line'></span>
-                    <p>A Intense é uma empresa de Planejamento Financeiro Internacional com escritórios em várias jurisdições ao redor do mundo. Trabalhar com a Intense me dá a capacidade de aconselhar expatriados internacionais que vivem no Oriente Médio de onde moro nos EUA.</p>
+                    <p>A Intense é uma empresa de Planejamento Financeiro Internacional com escritórios em várias jurisdições ao redor do mundo. Trabalhar com a Intense me dá a capacidade de aconselhar expatriados internacionais que vivem no Oriente Médio de onde moro no Brasil.</p>
                     <p>Sou John Doe, consultor sênior de uma empresa de planejamento financeiro independente chamada Intense.</p>
                     <button className='button'>Explore mais</button>
                 </div>
